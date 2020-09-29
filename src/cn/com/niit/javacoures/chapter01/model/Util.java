589a8a865675feac9a68420b7d94fdfdabe2c2ff
Kami <17865426360@163.com>
2020-09-29 15:48:33 +0800

    删除chapter01

--- src/cn/com/niit/javacoures/chapter01/model/Util.java
+++
@@ -1,5 +0,0 @@
-package cn.com.niit.javacoures.chapter01.model;
-
-public class Util {
-
-}
--- src/cn/com/niit/javacoures/chapter01/view/Util.java
+++
@@ -1,7 +0,0 @@
-package cn.com.niit.javacoures.chapter01.view;
-
-public class Util {
-	protected void fun() {
-		
-	}
-}
